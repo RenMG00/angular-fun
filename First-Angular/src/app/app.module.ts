43570036226b97f1658613bb22542c6c4b8dad5e
RenMG00 <laurengian@gmail.com>
2022-01-17 17:47:14 -0700

    first commit

---
+++ First-Angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { HomeComponent } from './home/home.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponentComponent,
+    HomeComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
